257d27a03c0409bea78768dcdf8b3bab91aead38
TBMeffordGIS <tbmefford@cox.net>
2026-06-27 15:14:59 -0700

    Update script.js

--- Interactive-Utility-Asset-Viewer/script.js
+++ Interactive-Utility-Asset-Viewer/script.js
@@ -7,6 +7,39 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors"
 }).addTo(map);
 
+// Custom utility icons
+const transformerIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
+const poleIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
+const hydrantIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
+const valveIcon = L.icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34]
+});
+
 // Sample utility assets
 const utilityAssets = [
     {
@@ -41,7 +74,17 @@ const utilityAssets = [
 
 // Add markers and popups
 utilityAssets.forEach(asset => {
-    L.marker(asset.coordinates)
+
+    let icon;
+
+switch (asset.type) {
+    case "Transformer": icon = transformerIcon; break;
+    case "Pole": icon = poleIcon; break;
+    case "Hydrant": icon = hydrantIcon; break;
+    case "Valve": icon = valveIcon; break;
+}
+
+L.marker(asset.coordinates, { icon: icon })
         .addTo(map)
         .bindPopup(`
             <strong>${asset.name}</strong><br>
